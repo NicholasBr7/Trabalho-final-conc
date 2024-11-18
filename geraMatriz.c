6b76df89382bbd74b95c742ca954c1ea34f3f071
NicholasBr7 <nicholasaf7@gmail.com>
2024-11-18 20:25:28 -0300

    Atualizado arquivo de gerar matriz e criada função para ler arquivo binário

--- geraMatriz.c
+++ geraMatriz.c
@@ -1,25 +1,34 @@
+/*
+    Esse arquivo serve gerar uma matriz com valores aleatórios inteiros 
+    entre -100 e 100. O número de colunas será sempre col + 1. 
+    Seu resultado será salvo em um arquivo binário e o outro txt
+
+    Entrada: ./nomeArquivo nLinhas nColunas 
+    Saída: arquivos binário e txt
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define TXT
+#define IMPRIME
 
 int main(int argc, char *argv[]) {
-    float *matriz; //matriz que será gerada
-    int linhas, colunas; //dimensoes da matriz
-    long long int tam; //qtde de elementos na matriz
-    FILE * descritorArquivo; //descritor do arquivo de saida
-    size_t ret; //retorno da funcao de escrita no arquivo de saida
+    float *matriz; // matriz que será gerada
+    int linhas, colunas; // dimensões da matriz
+    long long int tam; // qtde de elementos na matriz
+    FILE *descritorBinario, *descritorTexto; // descritores dos arquivos de saída
+    size_t ret; // retorno da função de escrita no arquivo de saída
 
     if (argc < 3) {
-        printf("Parâmetros inválidos. Padrão: <nomeArquivo> <nLinhas> <nColunas> <arquivoSaida> \n");
+        printf("Parâmetros inválidos. Padrão: <nLinhas> <nColunas> \n");
         return 1;
     }
     linhas = atoi(argv[1]);
     colunas = atoi(argv[2]) + 1;
     tam = linhas * colunas;
 
-    //aloca memoria para a matriz
+    // aloca memória para a matriz
     matriz = (float*) malloc(sizeof(float) * tam);
     if (!matriz) {
         printf("Erro de alocação de memória para a matriz");
@@ -30,34 +39,55 @@ int main(int argc, char *argv[]) {
         *(matriz + i) = (rand() % 201) - 100; // Gera números inteiros entre -100 e 100
     }
 
-    //imprimir na saida padrao a matriz gerada
-    #ifdef TXT
-    for(int i=0; i<linhas; i++) {
-        for(int j=0; j<colunas; j++)
-        fprintf(stdout, "%.6f ", matriz[i*colunas+j]);
-      fprintf(stdout, "\n");
-   }
-    #endif
-
-    //escreve a matriz no arquivo
-    //abre o arquivo para escrita binaria
-    descritorArquivo = fopen(argv[3], "wb");
-    if(!descritorArquivo) {
-      printf("Erro de abertura do arquivo");
-      return 3;
+    // imprimir na saída padrão e no arquivo de texto a matriz gerada
+    descritorTexto = fopen("matriz.txt", "w");
+    if (!descritorTexto) {
+        printf("Erro de abertura do arquivo de texto");
+        return 2;
     }
-    //escreve numero de linhas e de colunas
-    ret = fwrite(&linhas, sizeof(int), 1, descritorArquivo);
-    ret = fwrite(&colunas, sizeof(int), 1, descritorArquivo);
-    //escreve os elementos da matriz
-    ret = fwrite(matriz, sizeof(float), tam, descritorArquivo);
-    if(ret < tam) {
-        fprintf(stderr, "Erro de escrita no  arquivo\n");
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            float value = matriz[i * colunas + j];
+            if (value == (int)value) {
+                #ifdef IMPRIME
+                fprintf(stdout, "%d ", (int)value);
+                #endif
+                fprintf(descritorTexto, "%d ", (int)value);
+            } else {
+                #ifdef IMPRIME
+                fprintf(stdout, "%.6f ", value);
+                #endif
+                fprintf(descritorTexto, "%.6f ", value);
+            }
+        }
+        #ifdef IMPRIME
+        fprintf(stdout, "\n");
+        #endif
+        fprintf(descritorTexto, "\n");
+    }
+
+    fclose(descritorTexto);
+
+    // escreve a matriz no arquivo binário
+    descritorBinario = fopen("matriz.bin", "wb");
+    if (!descritorBinario) {
+        printf("Erro de abertura do arquivo binário");
+        return 3;
+    }
+    // escreve número de linhas e de colunas
+    fwrite(&linhas, sizeof(int), 1, descritorBinario);
+    fwrite(&colunas, sizeof(int), 1, descritorBinario);
+    // escreve os elementos da matriz
+    ret = fwrite(matriz, sizeof(float), tam, descritorBinario);
+    if (ret < tam) {
+        fprintf(stderr, "Erro de escrita no arquivo binário\n");
         return 4;
     }
 
-    //finaliza o uso das variaveis
-    fclose(descritorArquivo);
+    // finaliza o uso das variáveis
+    fclose(descritorBinario);
     free(matriz);
     return 0;
 }
+
