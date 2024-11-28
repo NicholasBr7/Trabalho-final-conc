cd887943a236885bc55b8345bbce0acfeb217f67
NicholasBr7 <nicholasaf7@gmail.com>
2024-11-28 17:45:38 -0300

    refatorizada funções da eliminação concorrente

--- elimConc.c
+++ elimConc.c
@@ -15,11 +15,13 @@
 #include <math.h>
 #include <pthread.h>
 #include <time.h>
+#include "functions.h"
+#include "functions.c"
 
 int nThreads;
+int bloqueadas = 0;
 pthread_mutex_t mutex;
 pthread_cond_t cond;
-int bloqueadas = 0;
 
 typedef struct {
     int id;           
@@ -41,67 +43,7 @@ void barreira(int nthreads) {
     pthread_mutex_unlock(&mutex); // Fim da seção crítica
 }
 
-double** ler_matriz_aumentada(const char* nome_arquivo, int* linhas, int* colunas) {
-    FILE* arquivo = fopen(nome_arquivo, "r");
-    if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo");
-        exit(EXIT_FAILURE);
-    }
-
-    *linhas = 0;
-    *colunas = 0;
-    double temp;
-
-    while (!feof(arquivo)) {
-        int count_colunas = 0;
-        char linha[50000];
-        if (fgets(linha, sizeof(linha), arquivo)) {
-            char* ptr = linha;
-            while (sscanf(ptr, "%lf%n", &temp, &count_colunas) == 1) {
-                ptr += count_colunas;
-                (*colunas)++;
-            }
-            (*linhas)++;
-        }
-    }
-    *colunas /= *linhas;
-
-    rewind(arquivo);
-
-    double** matriz = (double**)malloc(*linhas * sizeof(double*));
-    for (int i = 0; i < *linhas; i++) {
-        matriz[i] = (double*)malloc(*colunas * sizeof(double));
-    }
-
-    for (int i = 0; i < *linhas; i++) {
-        for (int j = 0; j < *colunas; j++) {
-            fscanf(arquivo, "%lf", &matriz[i][j]);
-        }
-    }
-
-    fclose(arquivo);
-
-    return matriz;
-}
-
-void liberar_matriz(double** matriz, int linhas) {
-    for (int i = 0; i < linhas; i++) {
-        free(matriz[i]);
-    }
-    free(matriz);
-}
-
-void substituicao_regressiva(double** matriz, int n, double* solucao) {
-    for (int i = n - 1; i >= 0; i--) {
-        solucao[i] = matriz[i][n];
-        for (int j = i + 1; j < n; j++) {
-            solucao[i] -= matriz[i][j] * solucao[j];
-        }
-        solucao[i] /= matriz[i][i];
-    }
-}
-
-void *eliminacao_gaussiana(void *arg) {
+void *eliminacao_gaussiana_conc(void *arg) {
     tArgs *args = (tArgs*) arg;
     int n = args->dim;
     double** matriz = args->matriz;
@@ -123,37 +65,6 @@ void *eliminacao_gaussiana(void *arg) {
     }
 
     pthread_exit(NULL);
-}
-
-void escreve_matriz_arquivo(int linhas, int colunas, double **matriz, double* solucao, const char *nomeArquivo) {
-    FILE *arquivo = fopen(nomeArquivo, "w");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return;
-    }
-    
-    fprintf(arquivo, "Matriz\n");
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            if(fabs(matriz[i][j]) < 1e-3){
-                fprintf(arquivo, "%.3lf", 0.000);
-            }
-            else{
-                fprintf(arquivo, "%.3lf", matriz[i][j]);
-            }
-            if (j < colunas - 1) {
-                fprintf(arquivo, " ");
-            }
-        }
-        fprintf(arquivo, "\n");
-    }
-
-    fprintf(arquivo, "\nSolucao\n");
-    for (int i = 0; i < linhas; i++) {
-        fprintf(arquivo, "%.3lf\n", solucao[i]);
-    }
-    
-    fclose(arquivo);
 }
 
 int main(int argc, char* argv[]) {
@@ -194,7 +105,7 @@ int main(int argc, char* argv[]) {
         args[i].id = i;
         args[i].dim = linhas;
         args[i].matriz = matriz;
-        if (pthread_create(&tid[i], NULL, eliminacao_gaussiana, (void*)&args[i])) {
+        if (pthread_create(&tid[i], NULL, eliminacao_gaussiana_conc, (void*)&args[i])) {
             fprintf(stderr, "Erro ao criar thread.\n");
             return 1;
         }
@@ -230,7 +141,3 @@ int main(int argc, char* argv[]) {
 
     return 1;
 }
-
-
-
-
